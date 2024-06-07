Fix: use covariance weighting for articulated body experiments and do not use noisy motion data for bunny.
For the former, the solutions should be unaffected by using the
weighting likely increases the solve times, which should therefore be
done for fairness. For the latter, we do not want to use the numerically
differentiated motion data, even if it does not have extra noise added
to it.

scripts/experiments/bunny/regress_wrl_data.py
@@ -14,7 +14,7 @@ import IPython
 
 
 TRAIN_TEST_SPLIT = 0.5
-
+TRAIN_WITH_NOISY_MOTION = False
 SHUFFLE = True
 REGULARIZATION_COEFF = 0
 PIM_EPS = 1e-4
@@ -63,16 +63,20 @@ def main():
         As = np.array(data["obj_data_full"][i]["As"])[idx, :]
         ws = np.array(data["obj_data_full"][i]["ws"])[idx, :]
 
-        n = Vs.shape[0]
-        n_train = int(TRAIN_TEST_SPLIT * n)
-
-        # regression/training data
+        # noisy data
         Vs_noisy = np.array(data["obj_data_full"][i]["Vs_noisy"])[idx, :]
         As_noisy = np.array(data["obj_data_full"][i]["As_noisy"])[idx, :]
         ws_noisy = np.array(data["obj_data_full"][i]["ws_noisy"])[idx, :]
 
-        Vs_train = Vs_noisy[:n_train]
-        As_train = As_noisy[:n_train]
+        n = Vs.shape[0]
+        n_train = int(TRAIN_TEST_SPLIT * n)
+
+        if TRAIN_WITH_NOISY_MOTION:
+            Vs_train = Vs_noisy[:n_train]
+            As_train = As_noisy[:n_train]
+        else:
+            Vs_train = Vs[:n_train]
+            As_train = As[:n_train]
         ws_train = ws_noisy[:n_train]
 
         Ys_train = np.array(
@@ -121,7 +125,6 @@ def main():
     with open("bunny_regression.pkl", "wb") as f:
         pickle.dump(regression_results, f)
     print("Saved results.")
-
 
     mean_func = np.median
     print("Identification")

scripts/experiments/single_body/plot_results.py
@@ -11,25 +11,22 @@ import numpy as np
 import IPython
 
 
-FIGURE_PATH = "figures/single_body_plot_100.pdf"
-# FIGURE_PATH = "figures/single_body_plot_1000.pdf"
+# FIGURE_PATH = "figures/single_body_plot.pdf"
+FIGURE_PATH = "/home/adam/phd/papers/rigeo/heins-wafr24/tex/current/figures/single_body_plot.pdf"
+
 
 DATA_DIR = Path("data")
 
 DATA_PATHS_FULL = [
     DATA_DIR / name
     for name in [
-        # "regress1000_wrench_bias0.pkl",
-        # "regress1000_wrench_bias1.pkl",
-        # "regress1000_wrench_bias5.pkl",
-        "regress_wrench_bias0.pkl",
-        "regress_wrench_bias_f1.pkl",
-        "regress_wrench_bias_f5.pkl",
-        # "regress_wrench_bias_f5_reg.pkl",
+        "results_bias0.pkl",
+        "results_bias1.pkl",
+        "results_bias5.pkl",
     ]
 ]
 
-CONSTRAINT_LABELS = ["Nominal", "Ellipsoidal", "Ell+CoM", "Polyhedral"]
+CONSTRAINT_LABELS = ["Nom", "Ell", "Ell+CoM", "CHE"]
 SCENARIO_LABELS = ["0", "1", "5"]
 
 NUM_CONSTRAINT_TYPES = len(CONSTRAINT_LABELS)
@@ -206,15 +203,15 @@ def plot_results():
     plt.rcParams.update(
         {
             "pgf.texsystem": "pdflatex",
-            "font.size": 6,
+            "font.size": 8,
             "font.family": "serif",
             "font.sans-serif": "DejaVu Sans",
             "font.weight": "normal",
             "text.usetex": True,
-            "legend.fontsize": 6,
-            "axes.titlesize": 6,
-            "axes.labelsize": 6,
-            "xtick.labelsize": 6,
+            "legend.fontsize": 8,
+            "axes.titlesize": 8,
+            "axes.labelsize": 8,
+            "xtick.labelsize": 8,
             "pgf.preamble": "\n".join(
                 [
                     r"\usepackage[utf8]{inputenc}",
@@ -228,7 +225,7 @@ def plot_results():
 
     palette = seaborn.color_palette("deep")
 
-    fig = plt.figure(figsize=(6.5, 1.75))
+    fig = plt.figure(figsize=(6, 1.75))
     label_xs = np.arange(NUM_SCENARIOS)
 
     ax1 = plt.subplot(1, 4, 1)
@@ -252,7 +249,7 @@ def plot_results():
     # hide_y_ticks(ax2)
 
     ax3 = plt.subplot(1, 4, 3)
-    ax3.set_title("Solve time [ms]")
+    ax3.set_title("Solve Time [ms]")
     solve_time_bar_data(ax3, full_results)
     ax3.grid(color=(0.75, 0.75, 0.75), alpha=0.5, linewidth=0.5, axis="y")
     ax3.tick_params(axis="x", length=0)
@@ -265,15 +262,17 @@ def plot_results():
     ax4.tick_params(axis="x", length=0)
     ax4.set_xticks(label_xs, SCENARIO_LABELS, rotation=0)
 
+    fig.supxlabel("Bias force $f_b$ [N]", x=0.3, y=0.02)
+
     fig.tight_layout(pad=0.1, w_pad=0.5)
     plt.subplots_adjust(bottom=0.2)
     plt.figlegend(
         handles,
         CONSTRAINT_LABELS,
         loc="lower center",
-        bbox_to_anchor=(0.565, 0),
+        bbox_to_anchor=(0.72, -0.02),
         ncols=len(CONSTRAINT_LABELS),
-        # handlelength=1,
+        handlelength=1,
     )
     fig.savefig(FIGURE_PATH)
     print(f"Saved figure to {FIGURE_PATH}")

scripts/experiments/single_body/regress.py
@@ -66,9 +66,6 @@ def main():
         help="Use bounding box rather than convex hull.",
         action="store_true",
     )
-    # parser.add_argument(
-    #     "--save", help="Save the results to a pickle.", action="store_true"
-    # )
     args = parser.parse_args()
 
     with open(args.infile, "rb") as f:
@@ -112,10 +109,7 @@ def main():
         As = np.array(data["obj_data_full"][i]["As"])[idx, :]
         ws = np.array(data["obj_data_full"][i]["ws"])[idx, :]
 
-        n = Vs.shape[0]
-        n_train = int(TRAIN_TEST_SPLIT * n)
-
-        # regression/training data
+        # noisy data
         if args.planar:
             Vs_noisy = np.array(data["obj_data_planar"][i]["Vs_noisy"])[idx, :]
             As_noisy = np.array(data["obj_data_planar"][i]["As_noisy"])[idx, :]
@@ -124,6 +118,9 @@ def main():
             Vs_noisy = np.array(data["obj_data_full"][i]["Vs_noisy"])[idx, :]
             As_noisy = np.array(data["obj_data_full"][i]["As_noisy"])[idx, :]
             ws_noisy = np.array(data["obj_data_full"][i]["ws_noisy"])[idx, :]
+
+        n = Vs.shape[0]
+        n_train = int(TRAIN_TEST_SPLIT * n)
 
         if TRAIN_WITH_NOISY_MOTION:
             Vs_train = Vs_noisy[:n_train]