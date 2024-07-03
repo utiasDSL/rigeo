a60b1d98f5d6307053e5f2b54a5f76383a345f6e
Adam Heins <mail@adamheins.com>
2024-07-03 13:32:35 -0400

    Add counter-example to conjectured cuboid trace constraints.

--- scripts/theory/cuboid.py
+++ scripts/theory/cuboid.py
@@ -20,4 +20,11 @@ masses3 = mass * np.ones(2) / 2
 points3 = np.array([[-0.5, 1, 2], [0.5, -1, -2]])
 params3 = rg.InertialParameters.from_point_masses(masses=masses3, points=points3)
 
+H = np.array([[0.2, 0, 0], [0, 0.9, 0.5], [0, 0.5, 3]])
+
+# 0.75 masses1 + 0.25 masses2 (linear combo of 1 and 2)
+masses4 = 0.75 * masses1 + 0.25 * np.array([0.25, 0, 0, 0.25, 0.25, 0, 0, 0.25])
+points4 = box.vertices
+params4 = rg.InertialParameters.from_point_masses(masses=masses4, points=points4)
+
 IPython.embed()
